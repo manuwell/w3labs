195c953824609b0b2c3e80a859d145190e00e821
Wellington Santos <wjsantos.wj@gmail.com>
2025-12-28 22:16:32 -0300

    main: aws-sdk connection and dynamodb table list

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,14 +1,20 @@
 package main
 
 import (
+	"context"
 	"log"
 	"w3labs/internal/adapters"
 	"w3labs/internal/config"
 
 	"github.com/caarlos0/env/v11"
+
+	awsConfig "github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
 )
 
 func main() {
+
+	ctx := context.Background()
 
 	var cfg config.Config
 	if err := env.Parse(&cfg); err != nil {
@@ -18,4 +24,23 @@ func main() {
 	logger := adapters.NewSlog(cfg.Logger)
 	logger.Debug("APPLICATION HAS STARTED", nil)
 
+	awsCfg, err := awsConfig.LoadDefaultConfig(ctx, func(o *awsConfig.LoadOptions) error {
+		if cfg.AwsEndpoint != "" {
+			o.BaseEndpoint = cfg.AwsEndpoint
+		}
+		return nil
+	})
+	if err != nil {
+		log.Fatalf("Failed to connecto to aws. %+v", err)
+	}
+
+	dynamodbClient := dynamodb.NewFromConfig(awsCfg)
+	tables, err := dynamodbClient.ListTables(ctx, &dynamodb.ListTablesInput{})
+	if err != nil {
+		log.Fatalf("Failed to list tables. %+v", err)
+	}
+
+	logger.Info("Tables: ", map[string]any{
+		"tables": tables,
+	})
 }
